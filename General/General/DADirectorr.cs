0cd6db147310c1ce0dc22099da7651c4a2ffa464
Melisa777 <iepvccardenas@gmail.com>
2017-07-21 23:00:40 -0500

    Base de datos

---
+++ General/General/DADirectorr.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace General
+{
+    class DADirectorr
+    {
+        static public List<Director> ListadoDirector()
+        {
+
+            List<Director> alumno = new List<Director>();
+            using (var data = new CineDBEntities())
+            {
+                return data.Director.ToList();
+            }
+        }
+    }
+}
