d5124913a0a5528e7d8bf5d0cd5bf7e54ab5a851
Melisa777 <iepvccardenas@gmail.com>
2017-07-21 23:19:30 -0500

    Examen

---
+++ General/EX_FINAL_02/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EX_FINAL_02.MvcApplication" Language="C#" %>
---
+++ General/EX_FINAL_02/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EX_FINAL_02.Startup))]
+namespace EX_FINAL_02
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
